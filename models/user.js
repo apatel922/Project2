805bab80df137888f70620d56c3449fe7bade56d
luke williams <jluke94williams@gmail.com>
2020-04-25 12:22:20 -0400

    be nice travis

--- models/user.js
+++ models/user.js
@@ -1,66 +1,66 @@
 // Requiring bcrypt for password hashing. Using the bcryptjs version as the regular bcrypt module sometimes causes errors on Windows machines
 var bcrypt = require("bcryptjs");
 // Creating our User model
-module.exports = function(sequelize, DataTypes) {
+module.exports = function (sequelize, DataTypes) {
   var User = sequelize.define("User", {
-      firstname: {
-        type: DataTypes.STRING, 
-        allowNull: false,
-      }
-    },
+    firstname: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    }
+  },
+  {
+    lastname: {
+      type: DataTypes.STRING,
+      allowNull: false
+    }
+  },
+  {
+    username: {
+      type: DataTypes.STRING,
+      allowNull: true
+    }
+  },
     {
-      lastname: {
-        type: DataTypes.STRING, 
-        allowNull: false
+    gradyear: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    }
+  },
+  {
+    profilepic: {
+      type: DataTypes.STRING,
+      allowNull: true
+    }
+  },
+  {
+    githuburl: {
+      type: DataTypes.STRING,
+      allowNull: false
+    }
+  },
+  {
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        isEmail: true
       }
-    },
-    {
-      username: {
-        type: DataTypes.STRING,
-        allowNull: true
-      }
-    },
-    {
-      gradyear: {
-        type: DataTypes.INTEGER,
-        allowNull: false
-      }
-    },
-    {
-      profilepic: {
-        type: DataTypes.STRING,
-        allowNull: true
-      }
-    },
-    {
-      githuburl: {
-        type: DataTypes.STRING,
-        allowNull: false
-      }
-    },
-    {
-      email: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        unique: true,
-        validate: {
-          isEmail: true
-        }
-      }
-    },
-    {
-      password: {
-        type: DataTypes.STRING,
-        allowNull: false
+    }
+  },
+  {
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false
     }
   });
   // Creating a custom method for our User model. This will check if an unhashed password entered by the user can be compared to the hashed password stored in our database
-  User.prototype.validPassword = function(password) {
+  User.prototype.validPassword = function (password) {
     return bcrypt.compareSync(password, this.password);
   };
   // Hooks are automatic methods that run during various phases of the User Model lifecycle
   // In this case, before a User is created, we will automatically hash their password
-  User.addHook("beforeCreate", function(user) {
+  User.addHook("beforeCreate", function (user) {
     user.password = bcrypt.hashSync(user.password, bcrypt.genSaltSync(10), null);
   });
   return User;
