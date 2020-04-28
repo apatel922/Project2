f0c5a26a61df3bba8c88de8c243c402674f75c43
Laura Armstrong <lauraearmstrong1@gmail.com>
2020-04-27 21:04:06 -0400

    started dynamic card

--- public/js/members.js
+++ public/js/members.js
@@ -1,7 +1,50 @@
-$(document).ready(function() {
+$(document).ready(function () {
   // This file just does a GET request to figure out which user is logged in
   // and updates the HTML on the page
-  $.get("/api/user_data").then(function(data) {
-    $(".member-name").text(data.email);
+  $.get("/api/user_data").then(function (userData) {
+
+    var first = userData.firstName;
+    var last = userData.lastName;
+    //var email = userData.email;
+    var gradYear = userData.gradYear;
+    //var userName = userData.userName;
+    //var pic = userData.profilePic
+    //var bio = userData.bio
+
+    //welcome header
+    var name = $("<h2>").text("Welcome, " + first + " " + last);
+    $(name).addClass("member-header");
+    $("body").append(name);
+
+
+    //profile card
+    var title = $(".card-title")
+    title.text(first + " " + last);
+    title.addClass("card-title")
+
+    var year = $(".class-year")
+    year.text(gradYear);
+    year.addClass("class-year")
+
+    var 
+
+    
+    
+    //$(".card-body").text(bio);
+    //$(".card-image").picture(pic);
+
+
   });
 });
+
+
+  // var userData = {
+  //   email: emailInput.val().trim(),
+  //   password: passwordInput.val().trim(),
+  //   firstName: firstname.val().trim(),
+  //   lastName: lastname.val().trim(),
+  //   gradYear: gradyear.val().trim(),
+  //   userName: username.val().trim(),
+  //   profilePic: "",
+  //   gitHubUrl:
+
