6947b14c5c2a85e8f5d532c3ec0071a2c620fb3a
Laura Armstrong <lauraearmstrong1@gmail.com>
2020-04-29 19:43:58 -0400

    fixed cards to start working

--- public/js/members.js
+++ public/js/members.js
@@ -1,7 +1,15 @@
 $(document).ready(function () {
   // This file just does a GET request to figure out which user is logged in
   // and updates the HTML on the page
-  //$.get("/api/user_data").then(function (userData) {
+
+  //welcome header
+  // $.get("/api/user").then(function (data) {
+  //   $(".member-name").text(data.firstname);
+  //  var name = $("<h2>").text("Welcome, " + results[results.length - 1].firstname);
+  //  $(name).addClass("member-header");
+  //  $("body").append(name);
+  // });
+
   function apiCall() {
     $.get("/api/userall", function (results) {
       console.log(results);
@@ -9,53 +17,60 @@ $(document).ready(function () {
     });
   }
 
-  function makeCard(members) {
-    for (var i = 0; i < members.length; i++) {
-      var first = members[i].firstname;
-      var last = members[i].lastname;
-      var gradYear = members[i].gradyear;
+  function makeCard(results) {
+    for (var i = 0; i < results.length; i++) {
+      var first = results[i].firstname;
+      var last = results[i].lastname;
+      var gradYear = results[i].gradyear;
       var bio = "test test test";
       var pic = $("<img>");
-      pic.src = "/public/placeholder.jpg";
-      //var email = members[i].email;
-      //var userName = members[i].username;
-
-      //welcome header
-      var name = $("<h2>").text("Welcome, " + members[members.length - 1].firstname);
-      $(name).addClass("member-header");
-      $("body").append(name);
-
+      pic.attr("src", "placeholder.jpg");
+      //var email = results[i].email;
+      //var userName = results[i].username;
 
       //profile card
-      // var div1 = $("<div>");
-      // div1.addClass("col s12 m6");
-      // $("body").append(div1)
+      var div1 = $("<div>");
+      div1.addClass("col s12 m6");
+      div1.attr("id", "div1");
+      
 
-      // var div2 = $("<div>");
-      // div2.addClass("card");
-      // div1.append(div2);
+      var div2 = $("<div>");
+      div2.addClass("card");
+      div2.attr("id", "div2");
+      
 
       var imgdiv = $("<div>");
       imgdiv.addClass("card-image");
-      div2.append(imgdiv);
-      div2.append(pic);
+      imgdiv.attr("id", "imgdiv");
+
+      pic.attr("id", "img1");
+      imgdiv.append(pic);
 
       //add button
 
       var div3 = $("<div>");
       div3.addClass("card-content");
-      div2.append(div3);
+      div3.attr("id", "div3");
+      
 
       var text1 = $("<span>").text(first + " " + last);
       text1.addClass("card-title");
+      text1.attr("id", "text1");
       div3.append(text1);
 
       var year = $("<span>").text(gradYear);
       year.addClass("class-year");
-      div3.append(year);
+      text1.append(year);
 
       var text2 = $("<p>").text(bio);
+      text2.attr("id", "text2");
       div3.append(text2);
+
+
+      div2.append(imgdiv);
+      div2.append(div3);
+      div1.append(div2);
+      $("#teamCards").append(div1);
 
     }
   }
