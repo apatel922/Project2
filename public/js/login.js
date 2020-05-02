6458e28c54424e81662755d27f6e8b198be4f088
Christian Balentine <christianbalentine47@gmail.com>
2020-05-02 13:14:00 -0400

    fixed login links

--- public/js/login.js
+++ public/js/login.js
@@ -1,16 +1,36 @@
-$(document).ready(function() {
+$(document).ready(function () {
   // Getting references to our form and inputs
-  var loginForm = $("form#login");
+  var loginForm = $("#login");
   var emailInput = $("input#email-input");
   var passwordInput = $("input#password-input");
 
+  function loginUser(email, password) {
+    console.log("Login User Call");
+
+    $.ajax("/api/login", {
+      data: {
+        email: email,
+        password: password
+      },
+      method: "POST",
+    })
+      .then(function (results) {
+        console.log(results);
+        // If there's an error, log the error
+      })
+      .catch(function (err) {
+        console.log(err);
+      });
+  }
+
   // When the form is submitted, we validate there's an email and password entered
-  loginForm.on("submit", function(event) {
+  loginForm.on("click", function (event) {
     event.preventDefault();
     var userData = {
       email: emailInput.val().trim(),
       password: passwordInput.val().trim()
     };
+    console.log(userData);
 
     if (!userData.email || !userData.password) {
       return;
@@ -23,17 +43,4 @@ $(document).ready(function() {
   });
 
   // loginUser does a post to our "api/login" route and if successful, redirects us the the members page
-  function loginUser(email, password) {
-    $.post("/api/login", {
-      email: email,
-      password: password
-    })
-      .then(function() {
-        window.location.replace("/members");
-        // If there's an error, log the error
-      })
-      .catch(function(err) {
-        console.log(err);
-      });
-  }
 });
